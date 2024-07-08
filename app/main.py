964e4700452a652c307a80c1788c4b87aeda97d4
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-07-08 15:43:25 +0530

    added comments

--- app/main.py
+++ app/main.py
@@ -37,60 +37,66 @@ def main():
     for i in range(length):
         c = file_contents[i]
         next_c = file_contents[i + 1] if i + 1 < length else None  # Peek at the next character
-        if c == "(":
-            print("LEFT_PAREN ( null")
-        elif c == ")":
-            print("RIGHT_PAREN ) null")
-        elif c == "{":
-            print("LEFT_BRACE { null")
-        elif c == "}":
-            print("RIGHT_BRACE } null")
-        elif c == "*":
-            print("STAR * null")
-        elif c == ".":
-            print("DOT . null")
-        elif c == ",":
-            print("COMMA , null")
-        elif c == "+":
-            print("PLUS + null")
-        elif c == "-":
-            print("MINUS - null")
-        elif c == ";":
-            print("SEMICOLON ; null")
-        elif c == "=": 
-            if count == 0: 
+        if countx == 0: 
+            if c == "(":
+                print("LEFT_PAREN ( null")
+            elif c == ")":
+                print("RIGHT_PAREN ) null")
+            elif c == "{":
+                print("LEFT_BRACE { null")
+            elif c == "}":
+                print("RIGHT_BRACE } null")
+            elif c == "*":
+                print("STAR * null")
+            elif c == ".":
+                print("DOT . null")
+            elif c == ",":
+                print("COMMA , null")
+            elif c == "+":
+                print("PLUS + null")
+            elif c == "-":
+                print("MINUS - null")
+            elif c == ";":
+                print("SEMICOLON ; null")
+            elif c == "=": 
+                if count == 0: 
+                    if next_c == "=": 
+                        print("EQUAL_EQUAL == null")
+                        count += 1 
+                    else : 
+                        print("EQUAL = null")
+                else : 
+                    count = 0 
+            elif c == "!": 
                 if next_c == "=": 
-                    print("EQUAL_EQUAL == null")
+                    print("BANG_EQUAL != null")
                     count += 1 
                 else : 
-                    print("EQUAL = null")
-            else : 
-                count = 0 
-        elif c == "!": 
-            if next_c == "=": 
-                print("BANG_EQUAL != null")
-                count += 1 
-            else : 
-                print("BANG ! null")
-        elif c == "<": 
-            if next_c == "=": 
-                print("LESS_EQUAL <= null")
-                count += 1 
-            else : 
-                print("LESS < null")
-        elif c == ">": 
-            if next_c == "=": 
-                print("GREATER_EQUAL >= null")
-                count += 1 
-            else : 
-                print("GREATER > null")
-        else:
-            error = True
-            line_number = file_contents.count("\n", 0, file_contents.find(c)) + 1
-            print(
-                "[line %s] Error: Unexpected character: %s" % (line_number, c),
-                file=sys.stderr,
-            )
+                    print("BANG ! null")
+            elif c == "<": 
+                if next_c == "=": 
+                    print("LESS_EQUAL <= null")
+                    count += 1 
+                else : 
+                    print("LESS < null")
+            elif c == ">": 
+                if next_c == "=": 
+                    print("GREATER_EQUAL >= null")
+                    count += 1 
+                else : 
+                    print("GREATER > null")
+            elif c == "/": 
+                if next_c == "/": 
+                    countx += 1
+                else : 
+                    print("SLASH / null")
+            else:
+                error = True
+                line_number = file_contents.count("\n", 0, file_contents.find(c)) + 1
+                print(
+                    "[line %s] Error: Unexpected character: %s" % (line_number, c),
+                    file=sys.stderr,
+                )
             
     print("EOF  null")
     if error:
@@ -101,7 +107,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-# LESS < null
-# LESS_EQUAL <= null
-# GREATER > null
-# GREATER_EQUAL >= null
+# SLASH / null
+
