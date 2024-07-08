23d83afe15e3f1fb1bb69d40cbf8ebfb27a5f9c2
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-07-08 16:16:34 +0530

    :(

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import sys
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
     if len(sys.argv) < 3:
@@ -19,23 +18,23 @@ def main():
         file_contents = file.read()
 
     error = False
-    length = len(file_contents)
     skip_next = False
     tokens = []
     error_messages = []
     line_number = 1
 
-    for i in range(length):
+    i = 0
+    while i < len(file_contents):
         if skip_next:
             skip_next = False
+            i += 1
             continue
 
         c = file_contents[i]
-        next_c = file_contents[i + 1] if i + 1 < length else None  # Peek at the next character
+        next_c = file_contents[i + 1] if i + 1 < len(file_contents) else None  # Peek at the next character
 
         if c == "\n":
             line_number += 1
-            continue
 
         if c == "(":
             tokens.append("LEFT_PAREN ( null")
@@ -83,14 +82,20 @@ def main():
                 tokens.append("GREATER > null")
         elif c == "/":
             if next_c == "/":
-                break  # Ignore the rest of the line for a comment
+                while i < len(file_contents) and file_contents[i] != "\n":
+                    i += 1
+                line_number += 1
             else:
                 tokens.append("SLASH / null")
         elif c == " " or c == "\t":
             pass  # Ignore whitespace characters
+        elif c == "\n":
+            pass  # Line number increment is already handled above
         else:
             error = True
-            error_messages.append("[line %s] Error: Unexpected character: %s" % (line_number, c))
+            error_messages.append("[line %d] Error: Unexpected character: %s" % (line_number, c))
+
+        i += 1
 
     tokens.append("EOF  null")
 
